481c2fbc0063bbd94515c862b96c261bfff43f07
JongMany <blackberry1114@naver.com>
2025-02-12 08:17:25 +0900

    docs: 테스트 디렉토리에서 적용이 되는지 파악

--- design-system/services/test/src/App.tsx
+++ design-system/services/test/src/App.tsx
@@ -1,10 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { ThemeProvider } from "@emotion/react";
+import { vars } from "@jm/themes";
+import styled from "@emotion/styled";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const theme = {
+    colors: vars.colors.$static.light,
+  };
+  return (
+    <ThemeProvider theme={theme}>
+      <View />
+    </ThemeProvider>
+  );
+}
+
+export default App;
+
+const View = () => {
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +44,19 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Text>텍스트!</Text>
+      <Text2>굿!</Text2>
     </>
-  )
-}
+  );
+};
 
-export default App
+const Text = styled.p`
+  color: ${({ theme }) => {
+    // @ts-ignore
+    return theme.colors.red[900];
+  }};
+`;
+
+const Text2 = styled.p`
+  color: ${vars.colors.$static.light.red[200]};
+`;
