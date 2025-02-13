7c7572cad66e6475161a118ed29fc1f64ae404b4
JongMany <blackberry1114@naver.com>
2025-02-13 22:49:57 +0900

    feat: layout 워크스페이스 추가

---
+++ design-system/packages/react/components/layout/src/Block.css.ts
@@ -0,0 +1,8 @@
+import {style} from "@vanilla-extract/css"
+import { vars } from '@jm/themes';
+
+export const blockStyle = style({
+  width: '100px',
+  height: '100px',
+  backgroundColor: vars.colors.$scale.blue[500],
+});
