f166e7f7a83d8d45f9f2376134475587fe4b5e09
JongMany <blackberry1114@naver.com>
2025-02-13 20:13:35 +0900

    feat: 빌드 스크립트 타이포그라피 추가

--- design-system/packages/theme/scripts/build-css-module.js
+++ design-system/packages/theme/scripts/build-css-module.js
@@ -29,7 +29,7 @@ const generateThemeCssVariables = () => {
             )
             .join("\n");
 
-          cssString.push(`${selector} {\n${cssVariables}\n}`);
+          return cssString.push(`${selector} {\n${cssVariables}\n}`);
         }
 
         if (colorKey === "dark") {
@@ -48,17 +48,56 @@ const generateThemeCssVariables = () => {
             )
             .join("\n");
 
-          cssString.push(`${selector} {\n${cssVariables}\n}`);
+          return cssString.push(`${selector} {\n${cssVariables}\n}`);
         }
       });
+      return;
     }
+
+    const selector = ":root";
+
+    const cssVariables = Object.entries(value)
+      .map(([mainKey, mainValue]) =>
+        Object.entries(mainValue)
+          .map(
+            ([subKey, subValue]) =>
+              `--${toCssCasting(mainKey)}-${toCssCasting(subKey)}: ${subValue};`
+          )
+          .join("\n")
+      )
+      .join("\n");
+
+    return cssString.push(`${selector} {\n${cssVariables}\n}`);
   });
   return cssString;
 };
 
+const generateThemeCssClasses = () => {
+  const cssString = [];
+
+  Object.entries(theme.classes).forEach(([key, value]) => {
+    const cssClasses = Object.entries(value)
+      .map(([mainKey, mainValue]) => {
+        const className = `.${toCssCasting(key)}${toCssCasting(mainKey)}`;
+
+        const styleProperties = Object.entries(mainValue)
+          .map(([subKey, subValue]) => {
+            return `${toCssCasting(subKey)}: ${subValue};`;
+          })
+          .join("\n");
+        return `${className} {\n${styleProperties}\n}`;
+      })
+      .join("\n");
+
+    cssString.push(cssClasses);
+  });
+
+  return cssString;
+};
 const generateThemeCss = () => {
   const variables = generateThemeCssVariables();
-  fs.writeFileSync("dist/themes.css", [...variables].join("\n"));
+  const classes = generateThemeCssClasses();
+  fs.writeFileSync("dist/themes.css", [...variables, ...classes].join("\n"));
 };
 
 generateThemeCss();
