d046ed75d96cebc0b6857e4e51c39dd9dce5ddf7
JongMany <blackberry1114@naver.com>
2025-02-17 20:50:44 +0900

    feat: Button 컴포넌트 및 스타일 추가

--- design-system/packages/react/components/button/src/Button.tsx
+++ design-system/packages/react/components/button/src/Button.tsx
@@ -1,11 +1,56 @@
 import * as React from "react";
 import { ButtonProps } from "./types";
+import { clsx } from "clsx";
+import { assignInlineVars } from "@vanilla-extract/dynamic";
+import {
+  activeColorVariant,
+  buttonStyle,
+  enableColorVariant,
+  hoverColorVariant,
+} from "./style.css";
+import { vars } from "@jm/themes";
 
 const Button = (props: ButtonProps, ref: React.Ref<HTMLButtonElement>) => {
-  const {children, ...rest} = props;
+  const {
+    variant = "solid",
+    size = "md",
+    color = "gray",
+    children,
+    style,
+  } = props;
 
-  return <button ref={ref} {...rest}>{children}</button>
-}
+  const enableColor = vars.colors.$scale[color][500];
+  const hoverColor =
+    variant === "solid"
+      ? vars.colors.$scale[color][600]
+      : vars.colors.$scale[color][50];
+  const activeColor =
+    variant === "solid"
+      ? vars.colors.$scale[color][700]
+      : vars.colors.$scale[color][100];
+
+  return (
+    <button
+      ref={ref}
+      className={clsx([
+        buttonStyle({
+          size,
+          variant,
+        }),
+      ])}
+      style={{
+        ...assignInlineVars({
+          [enableColorVariant]: enableColor,
+          [hoverColorVariant]: hoverColor,
+          [activeColorVariant]: activeColor,
+        }),
+        ...style,
+      }}
+    >
+      <span>{children}</span>
+    </button>
+  );
+};
 
 const _Button = React.forwardRef(Button);
-export {_Button as Button};
+export { _Button as Button };
---
+++ design-system/services/storybook/stories/ReactComponents/Button/Button.stories.tsx
@@ -0,0 +1,37 @@
+import "@jm/react-components-button/style.css";
+import { Button as _Button } from "@jm/react-components-button";
+import { vars } from "@jm/themes";
+
+export default {
+  title: "React Components/Button",
+  component: _Button,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    size: {
+      options: ["xs", "sm", "md", "lg"],
+      control: "select",
+    },
+    color: {
+      options: Object.keys(vars.colors.$scale),
+      control: "select",
+    },
+    variant: {
+      options: ["solid", "outline", "ghost"],
+      control: "select",
+    },
+  },
+};
+
+export const ButtonStory = {
+  args: {
+    size: "lg",
+    children: "Button",
+    variant: "outline",
+    isDisabled: false,
+    isLoading: false,
+    leftIcon: "😀",
+  },
+};
