c9e038299d552f2aa0c81bc81624458aff092c7a
JongMany <blackberry1114@naver.com>
2025-02-20 07:51:56 +0900

    feat: useInput (controlled, uncontrolled) 대응

--- design-system/packages/react/hooks/input/src/types.ts
+++ design-system/packages/react/hooks/input/src/types.ts
@@ -1,17 +1,18 @@
 import { ComponentPropsWithoutRef } from "react";
 
 export type UseInputProps = {
-  isDisabled?: boolean
-  isReadOnly?: boolean
-  isRequired?: boolean
-  isInvalid?: boolean
+  isDisabled?: boolean;
+  isReadOnly?: boolean;
+  isRequired?: boolean;
+  isInvalid?: boolean;
 } & Omit<ComponentPropsWithoutRef<"input">, "disabled" | "readOnly">;
 
 export type UseInputResult = {
   inputProps: ComponentPropsWithoutRef<"input"> & {
     "data-disabled": boolean;
-    "data-readonly": boolean;
-    "data-required": boolean;
     "data-invalid": boolean;
-  }
+    "aria-invalid": boolean;
+    "aria-required": boolean;
+  };
+  valueCount: number;
 };
