f7c836a71683a2df8b0365be133a1e415ba1d1c7
JongMany <blackberry1114@naver.com>
2025-02-21 07:54:46 +0900

    feat: Toast Component/Context/Provider

---
+++ design-system/packages/react/components/toast/src/ToastContainer.tsx
@@ -0,0 +1,10 @@
+import { PropsWithChildren } from "react";
+import { toastContainerStyle } from "./style.css";
+
+export const ToastContainer = ({ children }: PropsWithChildren<{}>) => {
+return (
+  <div id="toast-container" tabIndex={-1} className={toastContainerStyle}>
+    {children}
+  </div>
+);
+};
